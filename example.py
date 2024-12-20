8739646e12fd4493210ac31b23b9df5bc816627d
Hari <diego@robexia.com>
2024-12-20 01:45:54 +0100

    First class of the course

---
+++ example.py
@@ -0,0 +1,5 @@
+lista = [[1, 2, 3, [10, 20, 30, 40], ["Diego", "Hugo", "Javier", "Blanca"]]]
+
+print(lista[0][3][0])
+print("Esta es la segunda linea de codigo que estoy imprimiendo en el curso de pyhton, de 22 pavitos")
+
---
+++ introduction/helloWorld.py
@@ -0,0 +1,17 @@
+# Un lenguaje compilado es aquel que su codigo primero se traduce a lenguaje maquina y luego se ejecuta
+# Un lenguaje interpretado necesita un tercer programa para ponerse en marcha. En este caso el propio Visual Code
+
+print("Hi there my little muggots")
+
+# ------------ VARIABLES ----------------
+# Las variables son espacios reservados de almacenamiento, a los que se le asignan DATOS
+# Las variables NO inician con numeros.
+# No puede llevar caracteres especiales como ñ o @
+# No pueden llevar el nombre de palabras resrvadas de lenguaje, por ejemplo "print"
+
+# ---------- DATOS --------
+# Son la forma mínima de un programa, hay 3 principales (aunque no son los unicos)
+# Integers (enteros): no tienen parte decimal
+# Float (flotante): los que tienen decimales, como el pi, 3,1416
+# String (cadena de texto): Conjunto de varios caracteres de texto
+
